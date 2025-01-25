7dfda49cb0da53af29cfaff7a159e3442e08633a
brandon-schabel <brandonschabel1995@gmail.com>
2025-01-25 12:22:40 -0700

    updated .gitignore

--- packages/client/src/constants/server-constants.ts
+++ packages/client/src/constants/server-constants.ts
@@ -1,5 +1,5 @@
 export const SERVER_HOSTNAME = 'localhost'
-export const SERVER_PORT_DEV = 3000
+export const SERVER_PORT_DEV = 3001
 export const SERVER_PORT_PROD = 3579
 // in dev the client runs on port 5173 and the server runs on port 3000, in production the server servers the client (3579)
 export const SERVER_HTTP_ENDPOINT = process.env.NODE_ENV === "development" ? `http://${SERVER_HOSTNAME}:${SERVER_PORT_DEV}` : ""
