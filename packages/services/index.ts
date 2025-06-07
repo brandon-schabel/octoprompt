1836f7870b0e5baf1bc88d07b31626daa6fc7f46
brandon-schabel <brandonschabel1995@gmail.com>
2025-06-06 22:03:57 -0700

    update packages

--- packages/services/index.ts
+++ packages/services/index.ts
@@ -2,6 +2,7 @@ export * from './src/chat-service'
 export * from './src/project-service'
 export * from './src/prompt-service'
 export * from './src/provider-key-service'
+
 export * from './src/file-services/file-sync-service-unified'
 export * from './src/model-providers/model-fetcher-service'
 export * from './src/model-providers/provider-defaults'
