9c98a59f53e745c0ec65111d5cf47bebb2bd76f5
brandon-schabel <brandonschabel1995@gmail.com>
2025-08-03 23:11:01 -0700

    export fix

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -136,7 +136,7 @@ export {
   CollapsibleContent,
   ScrollArea,
   ScrollBar
-} from './components/data/index'
+} from './components/data'
 
 // Interaction components
 export { Slider } from './components/interaction/slider'
